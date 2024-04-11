8fdb2071928ef28f6735bc6628ddc8fc531da7f7
robertartress <robalan123@gmail.com>
2024-04-11 13:50:09 -0500

    added MFiT

--- playerData.js
+++ playerData.js
@@ -1,9 +1,14 @@
 var playerData = [
     {
-      title: 'Let It Ride [Master_v1] 44_16',
-      file: 'Let It Ride [Master_v1] 44_16.flac',
+      title: "Let It Ride [Master_v1] 44_16",
+      file: "Let It Ride [Master_v1] 44_16.flac",
       howl: null
-    }
+    },
+    {
+      title: "Let It Ride [Master_v1] (MFiT)",
+      file: "Let It Ride [Master_v1] (MFiT).flac",
+      howl: null
+    },
     // Add more tracks as needed
   ];
   
